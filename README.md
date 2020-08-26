05c9b61f181ecac99860e82fe9e1d50ec743b3f5
Ederson Ferreira <edersondeveloper@gmail.com>
2020-08-26 18:46:06 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,43 @@
+# PostMail
+
+PostMail is a program what you can very quickly send mails, written in plain text or markdown.
+
+# Install
+
+You can install PostMail with NPM, executing:
+
+`npm install postmail`
+
+# Usage
+
+First, you need to define your email, and your password. Run:
+
+`postmail --config`
+
+the prompt will ask your email and password, only complete your credentials normally.
+
+```
+Your Email: myemail@gmail.com
+Your Password: *********
+```
+
+## Plain Text
+
+Now, you can send a email, defining the mail/mails what you want to send your mail, the subject, and the plain text.
+
+`postmail --email anotherMail@gmail --subject Hello! --message like PostMail!`
+
+## Markdown
+
+You can create a markdown file, and send this markdown file to the PostMail, like this:
+
+`postmail --email anotherMail@gmail --subject Hello! --file myMarkdown.md`
+
+and the `MyMarkdown.md` can the any markdown file, with HTML tags or not, like this:
+
+```markdown
+# My Mail
+
+I like to send mails to another persons!
+```
+
---
+++ src/index.js
@@ -0,0 +1,72 @@
+const readlineSync = require('readline-sync');
+const nodeMailer = require('nodemailer');
+const md = require('markdown-it')({ html: true });
+const argv = require('minimist')(process.argv.slice(2));
+const fs = require('fs');
+
+const username = require('os').userInfo().username;
+
+// define the mail and password
+if (argv.config === true) {
+  const email = readlineSync.question('Your Email: ');
+  const password = readlineSync.question('Your Password: ', {
+    hideEchoBack: true,
+  });
+
+  data = `{
+    "email": "${email}",
+    "password": "${password}"
+}`;
+
+  fs.writeFileSync(`/home/${username}/.postmail.json`, data, 'utf8');
+} else {
+
+  // define global configs to send mail
+  
+  if (argv.emails != undefined && argv.subject != undefined) {
+    const settings = JSON.parse(
+      fs.readFileSync(`/home/${username}/.postmail.json`),
+    );
+
+    var transporter = nodeMailer.createTransport({
+      service: 'gmail',
+      auth: {
+        user: settings.email,
+        pass: settings.password,
+      },
+    });
+    
+    // use the plain text mail
+    if (argv.message != undefined) {
+      var mailOptions = {
+        from: settings.email,
+        to: argv.emails,
+        subject: argv.subject,
+        text: argv.message,
+      };
+    }
+    
+    // use the markdown mail
+    else {
+      if (argv.file != undefined) {
+        const file = fs.readFileSync(argv.file, 'utf8');
+        const htmlMessage = md.render(file);
+        var mailOptions = {
+          from: settings.email,
+          to: argv.emails,
+          subject: argv.subject,
+          html: htmlMessage,
+        };
+      }
+    }
+
+    // send mail
+    transporter.sendMail(mailOptions, function (error, info) {
+      if (error) {
+        console.log(error);
+      } else {
+        console.log('Email sent: ' + info.response);
+      }
+    });
+  }
+}
